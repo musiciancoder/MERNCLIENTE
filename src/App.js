8894e2ab43be870aced497296bc1bb71ffcc6910
musiciancoder <andresguitarra1980@gmail.com>
2020-08-12 11:02:05 -0400

    188.Creando los componentes y Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Login from "./components/auth/Login";
+import NuevaCuenta from "./components/auth/NuevaCuenta";
+import Proyectos from "./components/proyectos/Proyectos";
 
 //Proyecto iniciado con react-router-dom
 function App() {
   return (
-   <h1>MERN TASK</h1>
+   <Router>
+
+     <Switch>
+       <Route exact path="/" component={Login} />
+         <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+         <Route exact path="/proyectos" component={Proyectos} />
+
+
+     </Switch>
+
+   </Router>
   );
 }
 
---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NuevaCuenta = () => {
+    return(
+        <h1>Desde Nueacuenta</h1>
+    );
+}
+
+export default NuevaCuenta;
