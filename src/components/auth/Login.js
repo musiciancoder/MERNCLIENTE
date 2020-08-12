18f638ddb9857830f4a7f997fac5d0f505bbdf9b
musiciancoder <andresguitarra1980@gmail.com>
2020-08-12 11:42:31 -0400

    189.Creando formulario Login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,52 @@
 import React from 'react';
 
-const  Login= () => {
-    return(
-<h1>Desde Login</h1>
+const Login = () => {
+
+
+    const onChange = () => {
+
+    }
+
+    return (
+
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+
+
+                <form
+                    ///onSubmit={onSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            //value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            //     value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesión"/>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
