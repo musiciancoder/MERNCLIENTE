79e25f3a801189d4b9b26cd58681c6867a304623
musiciancoder <andresguitarra1980@gmail.com>
2020-08-15 19:32:39 -0400

    209. Ocultando el Formulario de Tareas si no hay Proyecto Seleccionado

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import ProyectoContext from "../../context/proyectos/proyectoContext";
+
 
 const FormTarea = () => {
-    return(
-<div className="formulario">
-    <form>
-        <div className="contenedor-input">
-            <input
-            type="text"
-            className="input-text"
-            placeholder="Nombre tarea"
-name="nombre"/>
-        </div>
 
-        <div className="contenedor-input">
-            <input
-                type="submit"
-                className="btn btn-primario btn-submit btn-block"
-                //value={tareaseleccionada ? 'Editar Tarea' : 'Agregar Tarea'}
-           value="agregar valor"
-            />
-        </div>
+    //Extraer si un proyecto esta activo
+    const proyectosContext = useContext(ProyectoContext);
+    const {proyecto} = proyectosContext;
 
-    </form>
-</div>
+    //Si no hay proyecto seleccionado no muestra el formulario de tareas
+    if (!proyecto) return null;
+
+    return (
+        <div className="formulario">
+            <form>
+                <div className="contenedor-input">
+                    <input
+                        type="text"
+                        className="input-text"
+                        placeholder="Nombre tarea"
+                        name="nombre"/>
+                </div>
+
+                <div className="contenedor-input">
+                    <input
+                        type="submit"
+                        className="btn btn-primario btn-submit btn-block"
+                        //value={tareaseleccionada ? 'Editar Tarea' : 'Agregar Tarea'}
+                        value="agregar valor"
+                    />
+                </div>
+
+            </form>
+        </div>
     );
 }
 
