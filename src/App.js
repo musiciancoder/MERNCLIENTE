8d5cfdaf9e59e8d6477eea622b4b95c903a998c0
musiciancoder <andresguitarra1980@gmail.com>
2020-08-26 11:27:36 -0400

    Reproducir vídeo
    1
    
    259. Creando el State de alertas

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ import NuevaCuenta from "./components/auth/NuevaCuenta";
 import Proyectos from "./components/proyectos/Proyectos";
 import ProyectoState from "./context/proyectos/proyectoState";
 import TareaState from "./context/tareas/tareaState";
+import AlertaState from "./context/alertas/alertaState"
 
 //Proyecto iniciado con react-router-dom
 function App() {
-  return (
-      //Esto equivale al <NombreProvider> del context normal
-      <ProyectoState>
-          <TareaState>
-   <Router>
+    return (
+        //Esto equivale al <NombreProvider> del context normal
+        <ProyectoState>
+            <TareaState>
+                <AlertaState>
+                    <Router>
 
-     <Switch>
-       <Route exact path="/" component={Login} />
-         <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-         <Route exact path="/proyectos" component={Proyectos} />
+                        <Switch>
 
+                            <Route exact path="/" component={Login}/>
+                            <Route exact path="/nueva-cuenta" component={NuevaCuenta}/>
+                            <Route exact path="/proyectos" component={Proyectos}/>
 
-     </Switch>
+                        </Switch>
 
-   </Router>
-          </TareaState>
-      </ProyectoState>
-  );
+                    </Router>
+                </AlertaState>
+            </TareaState>
+        </ProyectoState>
+    );
 }
 
 export default App;
---
+++ src/context/alertas/alertaContext.js
@@ -0,0 +1,8 @@
+import React, { createContext } from 'react';
+
+
+// Crear el Context
+const alertaContext = createContext();
+
+
+export default alertaContext;
